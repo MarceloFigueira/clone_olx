917bf54a1c97f827f1f25590b6adceb009192dcb
Marcelo Figueira <marcelofigueira87@gmail.com>
2022-10-31 22:00:02 -0300

    initial commit

---
+++ lib/home.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+class HomeWidget extends StatefulWidget {
+  const HomeWidget({Key? key}) : super(key: key);
+
+  @override
+  State<HomeWidget> createState() => _HomeWidgetState();
+}
+
+class _HomeWidgetState extends State<HomeWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
